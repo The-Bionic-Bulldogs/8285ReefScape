9d4db5c18dc0c250fd1debaa72b3750186b626a2
ncgears <1918ncgears@gmail.com>
2025-03-07 18:41:21 -0500

    Add tips to elevator test

--- src/main/java/frc/robot/subsystems/eleTest.java
+++ src/main/java/frc/robot/subsystems/eleTest.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.motorcontrol.Victor;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import com.ctre.phoenix6.sim.TalonFXSimState;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -13,7 +14,7 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 
 public class eleTest extends SubsystemBase {
-private TalonFX e_motor1 = new TalonFX(Constants.e_motor1);
+private TalonFX e_motor1 = new TalonFX(Constants.e_motor1);  //use m_somename... this is called a "member variable", ie, one that is local to this class
 private TalonFX e_motor2 = new TalonFX(Constants.e_motor2);
 
 private final ProfiledPIDController  elePIDController = new ProfiledPIDController
@@ -30,16 +31,30 @@ private final ElevatorFeedforward e_ElevatorFeedforward =
 TalonFXConfiguration eleConfiguration = new TalonFXConfiguration();
 //work in progress below
 public void runele(double speed){
-    if(speed > 1){
-    speed = 1;   
-    } 
-    if(speed < 1) { //this wont work, you are saying anything less than 1 is full reverse
-    speed = -1; //TIP: dont use integers when its actually a double.  ie. use -1.0 instead of -1
-    }
-    if(Math.abs(speed) > 0.1){
+    //looks like you are trying to limit the input to a max of 1, don't forget you need to handle -1 also...
+    // speed = Math.max(-1.0, Math.min(speed, 1.0)); //This makes absolutely sure it is between -1.0 and 1.0
+    speed = MathUtil.clamp(speed,-1.0,1.0); //This makes absolutely sure it is betwwen -1.0 and 1.0
+    
+    //it appears you were checking for a deadband value of 0.1.  This should be handled on the input side, rather than on the control side.
+    // however, if you wanted to do it, you could do
+    speed = MathUtil.applyDeadband(speed, 0.1);
+
+    //This works, but can be dangerous if something goes wrong. See ElevatorSubsystem.java for an example of creating a follower
+    //then, you can simply apply control only to the leader.
     e_motor1.set(speed);
     e_motor2.set(-speed);
-    } else stop();
+
+    // //your code
+    // if(speed > 1){
+    // speed = 1;   
+    // } 
+    // if(speed < 1) { //this wont work, you are saying anything less than 1 is full reverse
+    // speed = -1; //TIP: dont use integers when its actually a double.  ie. use -1.0 instead of -1
+    // }
+    // if(Math.abs(speed) > 0.1){
+    // e_motor1.set(speed);
+    // e_motor2.set(-speed);
+    // } else stop();
     
 }
 public void stop(){
