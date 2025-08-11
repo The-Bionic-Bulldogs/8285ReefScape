14a2788378383911642ff6e3210339928112736b
Trapper-Creates <bluedog367e@outlook.com>
2025-08-10 20:01:17 -0400

    "changed the elevator subsystem speed from 10 to 15"

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -5,17 +5,15 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.Constants;
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.units.measure.Angle;
 
 /**
  * This subsystem handles managing the Template.
@@ -69,7 +67,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     fxConfig.Slot0 = clConfigs;
 
     var motionConfig = new MotionMagicConfigs();
-    motionConfig.MotionMagicCruiseVelocity = 20.0 / 2.0; //20
+    motionConfig.MotionMagicCruiseVelocity = 20.0 - 5; //20
     motionConfig.MotionMagicAcceleration = 291;
     fxConfig.MotionMagic = motionConfig;
 
